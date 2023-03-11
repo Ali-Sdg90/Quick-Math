d208fe0e6623e83bd5cd4d8abfc565347075fe8f
Ali Sadeghi <ali909392@gmail.com>
2023-03-11 18:56:20 +0330

    Improve scss / Before add Side 2 and 3

---
+++ js/script.js
@@ -0,0 +1,17 @@
+changeColor();
+function changeColor() {
+    let rgbSaver = [];
+    for (let i = 0; i < 3; i++)
+        rgbSaver.push(Math.floor(Math.random() * 226 + 70));
+    document.querySelector(":root").style.cssText = `
+    --background-color: rgb(${rgbSaver[0]}, ${rgbSaver[1]}, ${rgbSaver[2]});
+    --game-color: rgb(${rgbSaver[0] / 2}, ${rgbSaver[1] / 2}, ${
+        rgbSaver[2] / 2
+    });
+    --line-color: rgb(${rgbSaver[0] / 3}, ${rgbSaver[1] / 3}, ${
+        rgbSaver[2] / 3
+    });
+    `;
+    console.log(document.querySelector(":root").style.cssText);
+    rgbSaver = [];
+}
