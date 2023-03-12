0c6c6480860f75f2ee3c6fc26bdfeb44de3650e0
Ali Sadeghi <ali909392@gmail.com>
2023-03-12 15:24:55 +0330

    Fix negative answers / Add disableEnableInputs() / Add submit-btn-effects

--- js/script.js
+++ js/script.js
@@ -8,59 +8,66 @@ const submitBtns = document.querySelectorAll(".submit-btn");
 const sideHealthcircles = document.querySelectorAll(".health-bar__blocks");
 const main = document.querySelector("main");
 const sideHealthLost = [0, 0, 0];
-let answer = ["", "", ""];
 
-for (let round = 0; round < 3; round++) {
-    let ransdNum = "0";
-    let calcStr = "";
-    do {
-        ransdNum = 100 - Math.ceil(Math.random() * 200);
-    } while (ransdNum === 0);
-    calcStr += ransdNum;
-    firstNumbers[round].textContent = ransdNum;
+setTimeout(() => {
+    setNumbers();
+}, 1000);
 
-    ransdNum = Math.ceil(Math.random() * 4);
-    switch (ransdNum) {
-        case 1:
-            ransdNum = "+";
-            break;
-        case 2:
-            ransdNum = "-";
-            break;
-        case 3:
-            ransdNum = "*";
-            break;
-        default:
-            ransdNum = "/";
-            break;
+function setNumbers() {
+    const answer = ["", "", ""];
+
+    for (let round = 0; round < 3; round++) {
+        let ransdNum = "0";
+        let calcStr = "";
+        do {
+            ransdNum = 100 - Math.ceil(Math.random() * 200);
+        } while (ransdNum === 0);
+        calcStr += ransdNum;
+        firstNumbers[round].textContent = ransdNum;
+
+        ransdNum = Math.ceil(Math.random() * 4);
+        switch (ransdNum) {
+            case 1:
+                ransdNum = "+";
+                break;
+            case 2:
+                ransdNum = "-";
+                break;
+            case 3:
+                ransdNum = "*";
+                break;
+            default:
+                ransdNum = "/";
+                break;
+        }
+        operations[round].textContent = ransdNum;
+        calcStr += ransdNum;
+
+        do {
+            ransdNum = 100 - Math.ceil(Math.random() * 200);
+        } while (ransdNum === "0");
+        calcStr += ransdNum;
+        secondNumbers[round].textContent = ransdNum;
+
+        calcStr = calc(calcStr.replace("--", "+"));
+        answer[round] = parseInt(calcStr);
+
+        submitBtns[round].addEventListener("click", function () {
+            let inputQstn = inputs[round].value;
+            if (!inputs[round].value) {
+                return;
+            }
+            console.log(inputQstn, answer[round]);
+            if (Number(inputQstn) === Number(answer[round])) {
+                console.log("yea!");
+                changeSide(round);
+            } else {
+                console.log("NOO!");
+                wrongAnimation(round);
+            }
+        });
+        console.log(calc(calcStr), answer[round]);
     }
-    operations[round].textContent = ransdNum;
-    calcStr += ransdNum;
-
-    do {
-        ransdNum = 100 - Math.ceil(Math.random() * 200);
-    } while (ransdNum === "0");
-    calcStr += ransdNum;
-    secondNumbers[round].textContent = ransdNum;
-
-    calcStr = calcStr.replace("--", "+");
-    answer[round] = Math.floor(calc(calcStr));
-
-    submitBtns[round].addEventListener("click", function () {
-        let inputQstn = inputs[round].value;
-        if (!inputs[round].value) {
-            return;
-        }
-        console.log(inputQstn, answer[round]);
-        if (Number(inputQstn) === Number(answer[round])) {
-            console.log("yea!");
-            changeSide(round);
-        } else {
-            console.log("NOO!");
-            wrongAnimation(round);
-        }
-    });
-    console.log(calcStr, calc(calcStr), Math.floor(calc(calcStr)));
 }
 
 function calc(str) {
@@ -69,19 +76,29 @@ function calc(str) {
 }
 
 function changeSide(round) {
-    console.log(typeof round);
     switch (round) {
         case 0:
             main.style.transform = "rotateX(260deg) rotateZ(-170deg)";
+            disableEnableInputs(round);
             break;
         case 1:
             main.style.transform =
-                "rotateX(182deg) rotateZ(-179deg) translate(0, -200px)";
+                "rotateX(185deg) rotateZ(-179deg) rotateY(5deg) translate(0, -200px)";
+            disableEnableInputs(round);
             break;
         default:
             endGameWin();
             break;
     }
+}
+
+function disableEnableInputs(round) {
+    submitBtns[round].classList.remove("submit-btn-effects");
+    submitBtns[round + 1].classList.add("submit-btn-effects");
+    submitBtns[round].disabled = true;
+    submitBtns[round + 1].disabled = false;
+    inputs[round].disabled = true;
+    inputs[round + 1].disabled = false;
 }
 
 function wrongAnimation(round) {
@@ -91,9 +108,7 @@ function wrongAnimation(round) {
     } else {
         sideHealthLost[round]++;
         console.log(sideHealthLost[round]);
-        // console.log(round * 3 + sideHealthLost[round]);
         for (let health = 0; health < sideHealthLost[round]; health++) {
-            // console.log("IN");
             sideHealthcircles[round * 3 + health].classList.add("lost-health");
         }
     }
